e77439bb389afd3d07c3e530430fc148ffbe02a4
apropername <feiyu131313@gmail.com>
2024-10-15 14:52:30 +0800

    优化MenuMgr中Menu容器数据结构

--- src/JLUDesignPattern/menu/MenuMgr.java
+++ src/JLUDesignPattern/menu/MenuMgr.java
@@ -1,26 +1,28 @@
 package JLUDesignPattern.menu;
 
-import java.util.Vector;
+import java.util.EnumMap;
+import java.util.Map;
 
-import static JLUDesignPattern.menu.MenuType.MAIN_MENU;
+import static JLUDesignPattern.menu.MenuType.*;
 
 public class MenuMgr {
     private static final MenuMgr instance=new MenuMgr();
     public static MenuMgr getInstance() {return instance;}
 
-    private Vector<Menu> mVctMenus = new Vector<>();
+    private Map<MenuType,Menu> mMapMenus = new EnumMap<>(MenuType.class);
     private MenuMgr() {
-        mVctMenus.add(new MainMenu());
-        mVctMenus.add(new NewMenu());
-        mVctMenus.add(new LoadMenu());
-        mVctMenus.add(new OptionMenu());
-        mVctMenus.add(new PlayMenu());
-        mVctMenus.add(new SaveMenu());
-        mVctMenus.add(new PauseMenu());
-        mVctMenus.add(new AnimateMenu());
-        mVctMenus.add(new MusicMenu());
-        mVctMenus.add(new PlayerCountMenu());
-        mVctMenus.add(new DifficultyMenu());
+        mMapMenus.put(MAIN_MENU,new MainMenu());
+        mMapMenus.put(NEW_MENU,new NewMenu());
+        mMapMenus.put(LOAD_MENU,new LoadMenu());
+        mMapMenus.put(OPTION_MENU,new OptionMenu());
+        mMapMenus.put(PLAY_MENU,new PlayMenu());
+        mMapMenus.put(SAVE_MENU,new SaveMenu());
+        mMapMenus.put(PAUSE_MENU,new PauseMenu());
+        mMapMenus.put(ANIMATE_MENU,new AnimateMenu());
+        mMapMenus.put(MUSIC_MENU,new MusicMenu());
+        mMapMenus.put(PLAYER_COUNT_MENU,new PlayerCountMenu());
+        mMapMenus.put(DIFFICULTY_MENU,new DifficultyMenu());
+
         setActivedMenu(MAIN_MENU);
 
     }
@@ -31,7 +33,7 @@ public class MenuMgr {
 
     private Menu mCurMenu = null; // 当前菜单
     public void setActivedMenu(MenuType menuType) {
-        mCurMenu = mVctMenus.get(menuType.ordinal());
+        mCurMenu = mMapMenus.get(menuType);
     }
 
 };
